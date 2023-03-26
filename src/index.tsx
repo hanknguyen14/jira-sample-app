72d20d80058e90b1db09fca8ee97a055ded799da
Nguyen Manh Hung <hungnguyen.dhg@gmail.com>
2023-03-26 09:53:57 +0700

    feat: implement issue panel

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
-import ForgeUI, { render, Fragment, Text, IssuePanel } from '@forge/ui';
+import ForgeUI, { render, Fragment, Text, IssuePanel, Image, Button, Code, Strong } from '@forge/ui';
 
 const App = () => {
   return (
     <Fragment>
-      <Text>Hello world!</Text>
+      <Text>
+        <Strong>Text prompt: </Strong>
+        <Code text="This is a text prompt" />
+      </Text>
+      <Image src="https://media.giphy.com/media/jUwpNzg9IcyrK/source.gif" alt="homer" />
     </Fragment>
   );
 };
